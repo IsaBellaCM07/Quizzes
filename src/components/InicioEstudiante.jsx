051dc4c57151ae7d70aeb6666889b5df02332e0c
Isabella Cardozo <isabella.cardozom@uqvirtual.edu.co>
2024-05-18 15:33:31 -0500

    Cositas 2.0 del estudiante

--- src/components/InicioEstudiante.jsx
+++ src/components/InicioEstudiante.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 import '../styles/InicioEstudianteStyle.css';
-import { useParams } from 'react-router-dom';
 
 const InicioEstudiante = () => {
     const { studentId } = useParams();
+    const navigate = useNavigate();
     const [student, setStudent] = useState(null);
     const [exams, setExams] = useState([]);
     const [presentedExams, setPresentedExams] = useState([]);
@@ -20,6 +21,7 @@ const InicioEstudiante = () => {
                 const examsResponse = await fetch(`http://localhost:3001/api/examsDis/${studentId}`);
                 const examsData = await examsResponse.json();
                 setExams(examsData);
+                console.log(examsData)
 
                 const presentedExamsResponse = await fetch(`http://localhost:3001/api/examsPres/${studentId}`);
                 const presentedExamsData = await presentedExamsResponse.json();
@@ -39,17 +41,23 @@ const InicioEstudiante = () => {
 
     return (
         <div className="dashboard-container">
-            <h1>Bienvenido/a, {student.NOMBRE}</h1>
+            <div className="back-arrow-container">
+                <FiArrowLeft className="back-arrow" onClick={() => navigate('/loginEstudiante')} />
+            </div>
+            <div className="text-container">
+                <h1>Hola, {student.NOMBRE}</h1>
+            </div>
 
             <div className="profile-section">
-                <h2>Perfil del Estudiante</h2>
+                <h2 className="text-titles">Perfil del Estudiante</h2>
                 <p><strong>Identificación:</strong> {student.ID_ESTUDIANTE}</p>
                 <p><strong>Nombre completo:</strong> {student.NOMBRE} {student.APELLIDO}</p>
+
             </div>
 
             <div className="exams-section">
-                <h2>Exámenes Disponibles</h2>
-                <ul>
+                <h2 className="text-titles">Exámenes Disponibles</h2>
+                <p>
                     {exams.map((exam, index) => (
                         <li key={index}>
                             <Link to={`/takeExam/${exam.ID_EXAMEN}`}>
@@ -59,12 +67,12 @@ const InicioEstudiante = () => {
                             </Link>
                         </li>
                     ))}
-                </ul>
+                </p>
             </div>
 
             <div className="presented-exams-section">
-                <h2>Exámenes Presentados</h2>
-                <ul>
+                <h2 className="text-titles">Exámenes Presentados</h2>
+                <p>
                     {presentedExams.map((exam, index) => (
                         <li key={index}>
                             <Link to={`/examResults/${exam.ID_EXAMEN}`}>
@@ -74,9 +82,8 @@ const InicioEstudiante = () => {
                             </Link>
                         </li>
                     ))}
-                </ul>
+                </p>
             </div>
-
         </div>
     );
 };
